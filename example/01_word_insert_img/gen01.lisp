842972b5261cb8fb1868806f201f42c2d3f5f1da
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-11-25 14:29:34 +0100

    sub seems to work

--- example/01_word_insert_img/gen01.lisp
+++ example/01_word_insert_img/gen01.lisp
@@ -14,28 +14,31 @@
 
   (write-source 
    (format nil "~a/source/01_word_insert_img" *path*)
-   `(
-     (do0
-      (setf
-       _code_git_version
-       (string ,(let ((str (with-output-to-string (s)
-			     (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
-		  (subseq str 0 (1- (length str)))))
-       _code_repository (string ,(format nil "https://github.com/plops/cl-vba-generator/tree/master/example/01_word_insert_img"))
-       _code_generation_time
-       (string ,(multiple-value-bind
-		      (second minute hour date month year day-of-week dst-p tz)
-		    (get-decoded-time)
-		  (declare (ignorable dst-p))
-		  (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
-			  hour
-			  minute
-			  second
-			  (nth day-of-week *day-names*)
-			  year
-			  month
-			  date
-			  (- tz)))))))))
+   `(do0
+     (setf
+      _code_git_version
+      (string ,(let ((str (with-output-to-string (s)
+			    (sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+		 (subseq str 0 (1- (length str)))))
+      _code_repository (string ,(format nil "https://github.com/plops/cl-vba-generator/tree/master/example/01_word_insert_img"))
+      _code_generation_time
+      (string ,(multiple-value-bind
+		     (second minute hour date month year day-of-week dst-p tz)
+		   (get-decoded-time)
+		 (declare (ignorable dst-p))
+		 (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			 hour
+			 minute
+			 second
+			 (nth day-of-week *day-names*)
+			 year
+			 month
+			 date
+			 (- tz)))))
+     (sub Question ()
+	  (setf myVar (MsgBox :Prompt (string "blabla")
+			      :Title (string "Answer Box")
+			      :Buttons (string "4")))))))
 
 
 
--- example/01_word_insert_img/source/01_word_insert_img.vba
+++ example/01_word_insert_img/source/01_word_insert_img.vba
@@ -1,4 +1,6 @@
-(_code_git_version="fatal: not a git repository (or any parent up to mount point /)
-Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set)."
+_code_git_version="c411ccc86cab797d3c51074d3ed6991e6432aded"
 _code_repository="https://github.com/plops/cl-vba-generator/tree/master/example/01_word_insert_img"
-_code_generation_time="14:24:19 of Thursday, 2021-11-25 (GMT+1)")()
+_code_generation_time="14:29:04 of Thursday, 2021-11-25 (GMT+1)"
+Sub Question()
+    myVar=MsgBox(Prompt:="blabla", Title:="Answer Box", Buttons:="4")
+End Sub
